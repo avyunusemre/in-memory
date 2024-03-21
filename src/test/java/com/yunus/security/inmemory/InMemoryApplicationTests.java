0c592f72cdd4f13d7eb50968936376c3c047c27f
avyunusemre <deu.av.yunusemre@gmail.com>
2024-03-21 15:36:52 +0100

    spring securtiy in memory

---
+++ src/test/java/com/yunus/security/inmemory/InMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunus.security.inmemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
